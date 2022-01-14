8acc6ee41f5fa8c9206f7aa0abb5b33620bb1a7e
Denish Rana <denish@venturetechlab.com>
2022-01-14 22:38:44 +0530

    feat: database connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,50 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const userModel = require("./models");
+mongoose.connect("mongodb://localhost:27017/react-test", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const PORT = process.env.PORT || 3010;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.post("/add_user", async (request, response) => {
+  const { name, age } = request.body;
+  const user = new userModel({
+    name,
+    age,
+  });
+
+  try {
+    await user.save();
+    response.send(user);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.get("/users", async (request, response) => {
+  const users = await userModel.find({});
+
+  try {
+    response.send(users);
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
 app.listen(PORT, () => {
