30edb7ed2634b9d0dad570e7aaca319f4817b266
Denish Rana <denish@venturetechlab.com>
2022-01-28 16:35:52 +0530

    feat: removed unnecessary things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { SearchInput } from "./components/searchInput";
 import "./App.scss";
 
 function App() {
   return (
     <div className="app-container">
-      <SearchInput />
+      Yo
     </div>
   );
 }
